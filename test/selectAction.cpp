c20093edf3efcc372f64e6de049505b445dbb85e
C0s1n <78977410+C0s1n@users.noreply.github.com>
2021-09-30 16:42:09 +0700

    Update selectAction.cpp

--- test/selectAction.cpp
+++ test/selectAction.cpp
@@ -20,3 +20,11 @@ int b = is_empty(in)
 int real2 = 0
 ASSERT_EQUAL(b, real2);
 }
+
+CTEST(selectAction, empty)
+{
+ifstream in("ip-016_password-generator/password.txt");
+int b = is_empty(in)
+int real2 = 1
+ASSERT_NOTEQUAL(b, real2);
+}
